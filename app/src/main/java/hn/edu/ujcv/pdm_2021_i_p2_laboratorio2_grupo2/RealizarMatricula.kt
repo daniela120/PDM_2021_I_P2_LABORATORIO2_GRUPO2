67cb04ae487982739ee6af94c9631526033c239c
Helen <este es https://github.com/edwinespPDM_2021_I_P1_PROYECTOGRUPO2.git>
2021-03-12 15:31:59 -0600

    aporte

--- app/src/main/java/hn/edu/ujcv/pdm_2021_i_p2_laboratorio2_grupo2/RealizarMatricula.kt
+++ app/src/main/java/hn/edu/ujcv/pdm_2021_i_p2_laboratorio2_grupo2/RealizarMatricula.kt
@@ -1,11 +1,17 @@
 package hn.edu.ujcv.pdm_2021_i_p2_laboratorio2_grupo2
 
 import android.net.Uri
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import kotlinx.android.synthetic.main.fragment_realizar_matricula.*
+
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -22,14 +28,44 @@ class RealizarMatricula : Fragment() {
     private var param1: String? = null
     private var param2: String? = null
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
             param1 = it.getString(ARG_PARAM1)
             param2 = it.getString(ARG_PARAM2)
         }
-    }
 
+
+
+
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val lista = resources.getStringArray(R.array.asignatus)
+
+        val adaptador = ArrayAdapter(this, android.R.layout.simple_spinner_item, lista)
+        spinner.adapter = adaptador
+
+        spinner.onItemSelectedListener = object :
+            AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+
+
+            }
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                txvSe1.text= lista[position].toString()
+            }
+
+        }
+
+        getAsignatura()
+    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -42,6 +78,12 @@ class RealizarMatricula : Fragment() {
         fun onFragmentInteraction(uri: Uri)
     }
 
+
+    fun getAsignatura() {
+        val bundle = intent.extras
+        val trans = bundle?.get("trans")
+        txvSe1.text = getString(R.string.seleccion, trans)
+    }
     companion object {
         /**
          * Use this factory method to create a new instance of
